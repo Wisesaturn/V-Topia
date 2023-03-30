343629968c57a1d06e26f5c833e94b8d9695cf62
Jae Han Song <rfv1479@gmail.com>
2023-03-31 02:14:40 +0900

    chore #6 : Jest CSS 파싱 오류

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '/\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/mock/fileMock.js',
-    '/\\.(css|less)$': '<rootDir>/src/mock/fileMock.js',
+    '\\.(css|less|svg)$': 'identity-obj-proxy',
   },
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['<rootDir>/**/*.test.js'],
