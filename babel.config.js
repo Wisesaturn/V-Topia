5b9508600e4401cd3b9f758d001b93e23e57fc8c
Jae Han Song <rfv1479@gmail.com>
2023-03-31 16:19:42 +0900

    Chore #7 : babel.config.js 설정
    - alias path 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,20 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { useBuiltIns: 'usage', corejs: '2', targets: { node: 'current' } }],
   ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@utils': './src/utils',
+          '@components': './src/components',
+          '@styles': './src/styles',
+          '@assets': './src/assets',
+          '@pages': './src/pages',
+          '@routes': './src/routes',
+          '@mock': './src/mock',
+        },
+      },
+    ],
+  ],
 };
