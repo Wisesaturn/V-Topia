c0a903c036b13ce8f0e1552144b51a717a9a5a9d
Jae Han Song <rfv1479@gmail.com>
2023-04-02 20:09:03 +0900

    Feat #11 : 앨범 flip 기능

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -10,6 +10,7 @@ class MainPage {
 
     this.render = () => {
       this.$container.innerHTML = this.contentHTML();
+      this.flip();
     };
 
     this.contentHTML = () => {
@@ -20,8 +21,8 @@ class MainPage {
             <div class="image">Image</div>
           </section>
           <section class="archiveSection">
-            <div class="title">
-              V-TOPIA ’s VIRTUAL ARTIST VIVIAN V-TOPIA ’s VIRTUAL ARTIST VIVIAN
+            <div class="archiveTitle" data-content="V-TOPIA ’s VIRTUAL ARTIST VIVIAN V-TOPIA ’s VIRTUAL ARTIST VIVIAN">
+              V-TOPIA ’s VIRTUAL ARTIST VIVIAN
             </div>
             <div class="subtitle">
               Archives
@@ -29,10 +30,20 @@ class MainPage {
             <div class="archivesWrapper">
               <div class="archives">
                 <div class="album">
-                  Album
+                  <div class="front">
+                    앞면 내용
+                  </div>
+                  <div class="back">
+                    뒷면 내용
+                  </div>
                 </div>
                 <div class="album">
-                  Album
+                  <div class="front">
+                    앞면 내용
+                  </div>
+                  <div class="back">
+                    뒷면 내용
+                  </div>
                 </div>
               </div>
             </div>
@@ -53,6 +64,26 @@ class MainPage {
           </section>
         </main>
       `;
+    };
+
+    this.flip = () => {
+      const albums = document.querySelectorAll('.album');
+
+      albums.forEach((album) => {
+        album.addEventListener('click', () => {
+          album.classList.toggle('flip');
+        });
+      });
+    };
+
+    this.unflip = () => {
+      const albums = document.querySelectorAll('.album');
+
+      albums.forEach((album) => {
+        album.removeEventListener('click', () => {
+          album.classList.toggle('flip');
+        });
+      });
     };
 
     this.render();
