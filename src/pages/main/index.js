d90e1e0a422aec6f3f0c21637ac25d6c47065ee6
Jae Han Song <rfv1479@gmail.com>
2023-04-20 19:29:11 +0900

    Feat #7 : Swiper 적용

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,14 +1,11 @@
 import './style.css';
 import { $ } from '@utils/querySelector';
 
-import Pagination from './components/pagination';
-
 class MainPage {
   constructor($container) {
     if (!$container) throw new Error('container is required');
 
     this.$container = $container;
-    this.pagination = '';
 
     this.setState = () => {
       this.render();
@@ -19,59 +16,68 @@ class MainPage {
       this.$container.innerHTML = this.contentHTML();
       this.useAlbumFlip();
       this.useVideoPlay();
-      if (document.getElementById('pagination')) this.pagination = new Pagination($('#pagination'));
     };
 
     this.contentHTML = () => {
       return `
         <main class="mainWrapper">
-          <section class="artistSection">
-            <div class="image">Image</div>
-            <div class="image">Image</div>
-          </section>
-          <section class="archiveSection">
-            <div class="archiveTitle" data-content="V-TOPIA ’s VIRTUAL ARTIST VIVIAN V-TOPIA ’s VIRTUAL ARTIST VIVIAN">
-              V-TOPIA ’s VIRTUAL ARTIST VIVIAN
-            </div>
-            <div class="subtitle">
-              Archives
-            </div>
-            <div class="archivesWrapper">
-              <div class="archives">
-                <div class="album">
-                  <div class="front">
-                    앞면 내용
-                  </div>
-                  <div class="back">
-                    뒷면 내용
+          <swiper-container direction="vertical" pagination="true" pagination-clickable="true" space-between="30" mousewheel="true">
+            <swiper-slide>
+            <section class="artistSection">
+              <div class="image">Image</div>
+              <div class="image">Image</div>
+              </section>
+            </swiper-slide>
+            <swiper-slide>
+              <section class="archiveSection">
+                <div class="archiveTitle" data-content="V-TOPIA ’s VIRTUAL ARTIST VIVIAN V-TOPIA ’s VIRTUAL ARTIST VIVIAN">
+                  V-TOPIA ’s VIRTUAL ARTIST VIVIAN
+                </div>
+                <div class="subtitle">
+                  Archives
+                </div>
+                <div class="archivesWrapper">
+                  <div class="archives">
+                    <div class="album">
+                      <div class="front">
+                        앞면 내용
+                      </div>
+                      <div class="back">
+                        뒷면 내용
+                      </div>
+                    </div>
+                    <div class="album">
+                      <div class="front">
+                        앞면 내용
+                      </div>
+                      <div class="back">
+                        뒷면 내용
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div class="album">
-                  <div class="front">
-                    앞면 내용
-                  </div>
-                  <div class="back">
-                    뒷면 내용
-                  </div>
+              </section>
+            </swiper-slide>
+            <swiper-slide>
+              <section class="videoSection">
+                <div class="title titleUp">
+                  <h1>VIVIAN TITLE</h1>
                 </div>
-              </div>
-            </div>
-          </section>
-          <section class="videoSection">
-            <div class="title titleUp">
-              <h1>VIVIAN TITLE</h1>
-            </div>
-            <div class="videoPlay"></div>
-            <div class="title titleDown">
-              <h1>PLAY ON CLICK</h1>
-            </div>
-          </section>
-          <section class="JooASection">
-            <h1>JooA</h1>
-            <div class="video">
-              video
-            </div>
-          </section>
+                <div class="videoPlay"></div>
+                <div class="title titleDown">
+                  <h1>PLAY ON CLICK</h1>
+                </div>
+              </section>
+            </swiper-slide>
+            <swiper-slide>
+              <section class="JooASection">
+                <h1>JooA</h1>
+                <div class="video">
+                  video
+                </div>
+              </section>
+            </swiper-slide>
+          </swiper-container>
         </main>
       `;
     };
@@ -115,7 +121,6 @@ class MainPage {
     this.unmount = () => {
       document.body.style.setProperty('overflow-y', 'scroll');
       this.$container.innerHTML = '';
-      if (this.pagination) this.pagination.unmount();
     };
 
     this.render();
