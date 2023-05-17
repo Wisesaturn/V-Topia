bc4b3ccff62e1fdd7f1926f4f679450acbea7647
Jae Han Song <rfv1479@gmail.com>
2023-05-17 20:02:12 +0900

    Feat #13 : display : none을 통한 클릭 에러 방지

--- src/app.js
+++ src/app.js
@@ -10,10 +10,18 @@ class App {
 
     const closedAllDropDown = () => {
       document.querySelectorAll('.dropDown').forEach((event) => {
-        if (!event.classList.contains('closed')) event.classList.add('closed');
+        if (!event.classList.contains('closed')) {
+          event.classList.add('closed');
+          setTimeout(() => {
+            event.classList.add('hidden');
+          }, 300);
+        } else {
+          event.classList.remove('hidden');
+        }
       });
     };
 
+    /* SPA */
     $('.link').addEventListener('click', (e) => {
       const target = e.target.closest('a');
       if (!(target instanceof HTMLAnchorElement)) return;
@@ -27,13 +35,18 @@ class App {
       dispatchEvent(new Event('popstate'));
     });
 
+    /* DropDown */
     document.querySelectorAll('.dropDownTitle').forEach((event) => {
       event.addEventListener('click', (e) => {
         const { target } = e;
-        if (!(target instanceof HTMLElement) || this.openDropDown) return;
+        if (!(target instanceof HTMLParagraphElement)) return;
 
-        closedAllDropDown();
-        $(`.dropDown${target.id}`).classList.toggle('closed');
+        if ($(`.dropDown${target.id}`).classList.contains('closed')) {
+          closedAllDropDown();
+          $(`.dropDown${target.id}`).classList.toggle('closed');
+        } else {
+          closedAllDropDown();
+        }
       });
     });
 
